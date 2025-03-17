b0ffc0046f90af428d0460b2bccd4d39c4ec6e7a
quincysorrentino <qsorrentino@wisc.edu>
2025-03-17 18:20:17 -0500

    added dateTime, weather

--- script.js
+++ script.js
@@ -1,21 +1,59 @@
 
-
-document.getElementById("sendButton").addEventListener("click", function() {
+function sendEmail(){
     let name = document.getElementById("nameEntry").value;
     let message = document.getElementById("messageEntry").value;
-
+    
     if (!name){
         alert("Please enter a name");
         return;
     }
-
+    
     if (!message){
         alert("Please enter a message");
         return;
     }
-
+    
     let subject = `Message from ${encodeURIComponent(name)}`;
-    let body = encodeURIComponent(message.replace(/\n/g, "%0A")); // Handle newlines
-
+    let body = encodeURIComponent(message.replace(/\n/g, "%0A"));
+    
     window.location.href = `mailto:qsorrentino@wisc.edu?subject=${subject}&body=${body}`;
-});
+    
+}
+
+function setDateTime(){
+    function updateTime(){
+        let now = new Date();
+        let formattedDateTime = now.toLocaleString();
+        document.getElementById("dateTime").textContent = formattedDateTime;
+    }
+
+    updateTime(); 
+    setInterval(updateTime, 1000); 
+}
+
+async function setWeather(){
+    try{
+    let response = await fetch('http://api.weatherapi.com/v1/current.json?key=547c60797bc34a28a19225506251703&q=Madison');
+    let data = await response.json();
+
+    console.log(data);
+
+    let cityName = data.location.name;
+    let temperature = data.current.temp_f;
+    let condition = data.current.condition.text;
+
+    document.getElementById("weather").textContent = `${cityName}, ${condition}, ${temperature}°F`
+    } catch (error){
+        alert("Error getting weather data")
+    }
+
+}
+
+window.onload = () => {
+    setDateTime();
+    setWeather();
+};
+
+document.getElementById("sendButton").addEventListener("click", sendEmail);
+
+
